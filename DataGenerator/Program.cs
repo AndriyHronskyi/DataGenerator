1106413b7c63f7524f1309e46bcf4ef1e773064a
AndriyHronskyi <Andrii.Hronskyi.work@gmail.com>
2022-09-04 21:32:31 +0300

    Created StreamRider. Add posibility read file, showing it to Console

--- DataGenerator/Program.cs
+++ DataGenerator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DataGenerator
 {
@@ -10,6 +11,33 @@ namespace DataGenerator
     {
         static void Main(string[] args)
         {
+            string Path1 = "E:\\Education\\DataRead\\fio.txt";
+            //string Path2 = "E:\\Education\\DataRead\\emails.txt";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(Path1, System.Text.Encoding.UTF8))
+                {
+                    string input = null;
+                    while ((input = sr.ReadLine()) != null)
+                    {
+                        //char c = input.ToCharArray()[0];
+
+
+
+                        Console.WriteLine(input);
+                    }
+                }
+                Console.ReadLine();
+            }
+            catch (FileNotFoundException e1)
+            {
+                Console.WriteLine(e1.Message);
+            }
+            catch (Exception e2)
+            {
+                Console.WriteLine(e2.Message);
+            }
         }
     }
 }
