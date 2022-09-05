3a80d2381a0573ab9e1b1b870874d509ab036e1d
AndriyHronskyi <Andrii.Hronskyi.work@gmail.com>
2022-09-05 15:08:29 +0300

    Formating First, Last name and Patronymic into querry. The output displayed into Console. It`s temporary solution!

--- DataGenerator/Program.cs
+++ DataGenerator/Program.cs
@@ -19,15 +19,70 @@ namespace DataGenerator
                 using (StreamReader sr = new StreamReader(Path1, System.Text.Encoding.UTF8))
                 {
                     string input = null;
+                    var koma = 1;
+
                     while ((input = sr.ReadLine()) != null)
                     {
-                        //char c = input.ToCharArray()[0];
+                        char[] outFIO = new char[input.Length];
+                        
+                        bool _fFlag = false;
+                        bool _sFlag = false;
 
+                        //open VALUE
+                        if (koma == 1)
+                        {
+                            Console.Write("('");
+                            koma++;
+                        }
+                        else
+                        {
+                            Console.Write(",('");
+                        }
 
+                        using (StringReader sr2 = new StringReader(input))
+                        {
+                            
+                            for (int i = 0; i < outFIO.Length; i++)
+                            {
 
-                        Console.WriteLine(input);
+                                /*if (_fFlag == _sFlag == false)
+                                {
+                                    sr2.Read(outFName, i, 1);
+                                }
+                                else if (_fFlag == true && _sFlag == false)
+                                {
+                                    sr2.Read(outLName, i, 1);
+                                }
+                                */
+                                sr2.Read(outFIO, i, 1);
+
+                                if (Char.IsSeparator(outFIO[i]))
+                                {
+                                    /*Console.Write("-");
+                                    if (_fFlag == false)
+                                    {
+                                        _fFlag = true;
+                                    }
+                                    else if (_fFlag == true)
+                                    {
+                                        _sFlag = true;
+                                    }*/
+
+                                    Console.Write("', '");
+                                }
+                                else
+                                {
+                                    Console.Write(outFIO[i]);
+                                }
+                            }
+                            
+                            
+                        }
+                        Console.Write("')");   //close VALUE
+                        Console.WriteLine();
                     }
                 }
+                
                 Console.ReadLine();
             }
             catch (FileNotFoundException e1)
