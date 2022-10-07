9b4426823bcb78427aec453d2982c072d93564de
AndriyHronskyi <Andrii.Hronskyi.work@gmail.com>
2022-10-08 01:36:51 +0300

    Add primitive parameter configure

--- DataGenerator/Program.cs
+++ DataGenerator/Program.cs
@@ -124,8 +124,8 @@ namespace DataGenerator
             #endregion
 
             #region New realisation
-
-            string[] Paths = new string[10];  //unitary container for multiple file paths
+            int maxFilesNumb = 10;
+            string[] Paths = new string[maxFilesNumb];  //unitary container for multiple file paths
             
             
             List<KeyValuePair<string, int>> TypesInput = new List<KeyValuePair<string, int>>()
@@ -136,10 +136,10 @@ namespace DataGenerator
                 new KeyValuePair<string, int>("Email", 2),
                 new KeyValuePair<string, int>("Login", 2),
             };        //What categories data we have in each file
-
-            List<KeyValuePair<string, string>> TypesOutput = new List<KeyValuePair<string, string>>()
+            /*
+            List<KeyValuePair<string, string[]>> TypesOutput = new List<KeyValuePair<string, string[]>>()
             {
-                new KeyValuePair<string, string>("FirstName", "read"),
+                new KeyValuePair<string, string[]>("FirstName", new string[] { "read", "read", "read"}), //value can be an array of some data
                 new KeyValuePair<string, string>("LastName", "read"),
                 new KeyValuePair<string, string>("Patronymic", "read"),
                 new KeyValuePair<string, string>("Email", "create"),
@@ -147,23 +147,56 @@ namespace DataGenerator
                 new KeyValuePair<string, string>("Password", "generate"),
                 new KeyValuePair<string, string>("Date", "generate")
             };   //What we should do (generate, read or create) with every output category
-
+            
             TypeData.Types = TypesOutput;
+            */
 
-
-
-
-            #endregion
-
-            #region User Console
+            //UI______________________________
             
             Console.WriteLine("Hello! This is a program for generating multiple strings querry.");
             Console.WriteLine("*");
             Console.WriteLine("*");
-            Console.WriteLine("1. Write full paths of files and parameters which they contain \n (It can be a names of fields in database)");
+
+            //Add configure generator querry
+            //What we should do (generate, read or create) with every output category
+
+            //Можна використати
+            //Зробити автоматичну передачу параметрів
+            //TypeData Td = new TypeData();
+            //Td.Types.Add(new KeyValuePair<string, string>("FirstName1", "read"));
+
+
+            Console.WriteLine("1. Configure parameters for generator querry");
+            Console.WriteLine("* \n Field name(in DB) can be a parameter. \n" +
+                "  There a 3 type of generation: \n" +
+                "   -read (this parameter would reded from files)\n" +
+                "   -create (this file would read from files and could be processed. Like login, email creates from FirstName, last name which could be in file)\n" +
+                "   -generate (randomly generates fome data. Like Date, Password, ID)\n \n");
+            int index = 0;
+            string buf1 = "";
+            string buf2 = "";
+            do
+            {
+                
+                Console.Write($"\n1.{index+1} Parameter(field in DB) name:");
+                    buf1 = Convert.ToString(Console.ReadLine());
+                
+                Console.Write($"1.{index+1} Type of generation:");
+                    buf2 = Convert.ToString(Console.ReadLine());
+
+                //Console.WriteLine("\n{0}-{1}.", buf1, buf2);
+                index++;
+                //TypesOutput.
+            } while (index < 5);
+
+
+
+
+            //Files Add
+            Console.WriteLine("\n2. Write full paths of files and parameters which they contain \n (It can be a names of fields in database)");
             Console.WriteLine("*");
 
-            int index = 0;
+            index = 0;
             do
             {
                 bool exitCondition = false;
@@ -189,7 +222,7 @@ namespace DataGenerator
                 }
                 index++;
 
-                Console.WriteLine($"1.{index } File path:");
+                Console.WriteLine($"2.{index } File path:");
                 string buf = Convert.ToString(Console.ReadLine());
                 //checking to Copy
                 if (index > 1)
@@ -198,7 +231,7 @@ namespace DataGenerator
                     {
                         Console.WriteLine("\n You have already aded path for this file!!!!");
                         Console.WriteLine("Add another file path, or press Enter - to next stage");
-                        Console.WriteLine($"1.{index} File path:");
+                        Console.WriteLine($"2.{index} File path:");
                         buf = Convert.ToString(Console.ReadLine());
                         if (buf.Equals(""))
                         {
@@ -214,8 +247,20 @@ namespace DataGenerator
                
                 Paths[index - 1] = buf;   //add file path
 
+                /*
+                 * TypesInput!!! 
+                 * Display which parametrs this file could have
+                 *  (серед тих параметрів, які були попередньо введені в typesOutput з станом Read 
+                 *  і не були присвоєні іншим файлам)
+                 * 
+                 * 
+                 * 
+                 * **/
+
                 Console.WriteLine("");
-            } while (index < 11);
+            } while (index < maxFilesNumb);
+
+
 
             Console.WriteLine("-");
             Console.WriteLine("-");
@@ -235,6 +280,8 @@ namespace DataGenerator
             Paths[0] = Path1;
             Paths[1] = Path2;
 
+
+            //Можна використати
             //Зробити автоматичну передачу параметрів
             //TypeData Td = new TypeData();
             //Td.Types.Add(new KeyValuePair<string, string>("FirstName1", "read"));
